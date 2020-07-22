dd3e49fc8a2ce08acee2cbe65adc1cd4c2ef0ab1
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-07-23 07:23:11 +0900

    Create Enum2.cs

---
+++ DayStudy/Enum2.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Enum2
+{
+  class MainClass 
+  {
+    enum DialogResult{YES, NO, CANCEL, CONFIRM, OK}
+
+    static void Main (string[] args) 
+    {
+      DialogResult result=DialogResult.YES;
+
+      Console.WriteLine (result == DialogResult.YES);
+      Console.WriteLine (result == DialogResult.NO);
+      Console.WriteLine (result == DialogResult.CANCEL);
+      Console.WriteLine (result == DialogResult.CONFIRM);
+      Console.WriteLine (result == DialogResult.OK);
+    }
+  }
+}
+
